aea852148e548656c74f84a5cbe990cd4f50c192
ilz-code <83577625+ilz-code@users.noreply.github.com>
2021-09-12 18:40:34 +0300

    Corrected

--- sql-shopify/test/05-integrity.test.ts
+++ sql-shopify/test/05-integrity.test.ts
@@ -56,9 +56,9 @@ describe("Foreign Keys", () => {
     it("should be able to delete app", async done => {
         const appId = 355;
         const query = `
-        delete from app_categories where app_id = 355 
-        and delete from app_pricing_plans where app_id =355 
-        and delete from apps where id = ${appId}`;
+        DELETE FROM apps_categories WHERE app_id = 355;
+        DELETE FROM apps_pricing_plans WHERE app_id = 355;
+        DELETE FROM apps WHERE id = 355`;
         try {
             await db.delete(query);
           } catch (e) {}
